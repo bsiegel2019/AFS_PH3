743cbdad6bedd1fc195f9b9ccd5ab96131d816e1
BSiegel <siegelbernard@yahoo.com>
2019-12-03 16:09:18 -0600

    renamed rest ctrlr to async and chged to @Controller still works ok

--- DronesCrashNBurn/src/com/drones/controller/DroneAsyncController.java
+++ DronesCrashNBurn/src/com/drones/controller/DroneAsyncController.java
@@ -6,16 +6,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.drones.model.Drone;
 import com.drones.service.DroneService;
 
-@RestController
-public class DroneRestController {
+@Controller
+public class DroneAsyncController {
 
 	@Autowired
 	DroneService droneService; // Service which will do all data retrieval/manipulation work
